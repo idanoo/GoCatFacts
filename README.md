d39591b3c0816ec733cf55e839cafd9ab29a6ada
Daniel Mason <daniel@m2.nz>
2025-06-14 19:36:43 +1200

    Working proof of concept

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # GoCatFacts
 
 Beautiful program. HTTP server. You hit? You get fact. Boom. Default port 80.
+Quick and dirty proof of concept.
 
 ```shell
 go run main.go
 ```
+
+Initial fact list copied from https://github.com/0x27/catfacts/blob/master/catfacts.txt
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
-package gocatfacts
+package main
 
-import "os"
+import (
+	"bufio"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+)
 
 var (
-	port = "8080"
+	port  = "8080"
+	facts = []string{}
 )
 
 func main() {
@@ -13,4 +21,42 @@ func main() {
 		port = portOverride
 	}
 
+	// Load it up
+	err := loadFacts()
+	if err != nil {
+		log.Fatalf("Error loading facts: %v", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, getRandomFact())
+	})
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Error loading facts: %v", err)
+	}
+}
+
+// loadFacts loads cat facts from a file named "facts.txt"
+func loadFacts() error {
+	file, err := os.Open("facts.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		facts = append(facts, scanner.Text())
+	}
+
+	return scanner.Err()
+}
+
+// getRandomFact returns a random cat fact from the loaded facts
+func getRandomFact() string {
+	if len(facts) == 0 {
+		return "No facts available."
+	}
+
+	return facts[rand.Intn(len(facts))]
 }
